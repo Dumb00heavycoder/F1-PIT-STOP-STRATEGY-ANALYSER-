1c61cc588a153bb852752f4f4a37b301ec50864a
Dumb00heavycoder <dhruvlgd44@gmail.com>
2025-06-06 16:39:06 +0530

    starting

---
+++ Mini_versions/Version_0.1.py
@@ -0,0 +1,8 @@
+#For the 0.1 version we will consider the lap time of each tyre to be specific 
+#We will only use 3 tyres in 2 pit strategy 
+
+#Soft= 10 laps
+#Medium = 20 laps
+#Hard = 30 laps 
+
+laps = input("Enter the amount of laps:- ")
